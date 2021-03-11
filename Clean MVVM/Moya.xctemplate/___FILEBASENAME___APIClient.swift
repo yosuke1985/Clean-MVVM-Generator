d0f3db8f53a30cb456a07b8cfff36f726fc6bfcf
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-03-11 23:11:48 +0900

    update

---
+++ Clean MVVM/Moya.xctemplate/___FILEBASENAME___APIClient.swift
@@ -0,0 +1,58 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import Foundation
+import Moya
+
+enum ___VARIABLE_sceneName___APIClient {
+    case getHoge(userId: String)
+}
+
+extension ___VARIABLE_sceneName___APIClient: TargetType {
+    var baseURL: URL {
+        return "http://www.hoge.com"
+    }
+    
+    var path: String {
+        switch self {
+        case .getHoge(userId: let userId):
+            return "hoge/hoge"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getHoge(userId: _):
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .getHoge(userId: let userId):
+            return .requestPlain
+//            return .requestParameters(parameters: ["":""])
+        }
+    }
+    
+    var sampleData:Data {
+        switch self {
+        case .getHoge(userId: _):
+            let path = Bundle.main.path(forResources: "Filename", ofType: "json")
+            
+            return Filehandle(forReadingAtPath: path)!.readDataToEndOfFile()
+        }
+    }
+    
+    var headers: [String:String]? {
+//        switch self {
+//        case .getHoge(userId: _):
+//            return ["Authorization": "Bearer \(token)"]
+//        }
+        return nil
+    }
+}
